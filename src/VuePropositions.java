f3fff42b199a3f860a51878aa5be259fb55cb5ec
RemyGizard <130280613+RemyGizard@users.noreply.github.com>
2024-03-07 09:42:30 +0100

    mise a jour de la vu

--- src/VuePropositions.java
+++ src/VuePropositions.java
@@ -11,17 +11,25 @@ public class VuePropositions extends JPanel {
          
         int size= 100/modele.DIFFICULTE;
         g.drawRect(0, 0, 200, 500);
+        
+        g.setColor(Color.BLACK);
+        g.drawRect(0, 0, 200, 500);
         for (int y = 0; y < 10; y++) {
+        	System.out.println(modele.propositions[y]);
         	if ((modele.propositions[y] != null)) {
+      
         		for (int x=0; x < modele.DIFFICULTE; x++) {
-        			g.setColor(modele.propositions[y].getColor(x));
-                    g.drawOval(10+x*size*2, 10+y*size*2, size, size);
-        			
+        			if (modele.propositions[y].getColor(x)!=null) {
+        				g.setColor(modele.propositions[y].getColor(x));
+                        g.fillOval(10+x*size*2, 10+y*size*2, size, size);
+        			}
         		}
         	}
+        	
         
         }
-        g.drawRect(0, 0, 200, 500);
+        System.out.println("--------------------------------------------------------------");
+        
       }
      
     public VuePropositions(Modele modele) {
