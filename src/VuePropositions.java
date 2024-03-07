999876f820cc331e16d82ee938a033e4c544eee9
meriem-meryy <152922527+meriem-meryy@users.noreply.github.com>
2024-03-07 10:02:34 +0100

    Update VuePropositions

--- src/VuePropositions.java
+++ src/VuePropositions.java
@@ -6,31 +6,23 @@ public class VuePropositions extends JPanel {
     Color[][] couleur_jouer;
      
      
-    public void paint(Graphics g){
-         
-         
-        int size= 100/modele.DIFFICULTE;
-        g.drawRect(0, 0, 200, 500);
-        
+    public void paint(Graphics g) {
+        int size = 100 / modele.DIFFICULTE;
         g.setColor(Color.BLACK);
         g.drawRect(0, 0, 200, 500);
+
         for (int y = 0; y < 10; y++) {
-        	System.out.println(modele.propositions[y]);
-        	if ((modele.propositions[y] != null)) {
-      
-        		for (int x=0; x < modele.DIFFICULTE; x++) {
-        			if (modele.propositions[y].getColor(x)!=null) {
-        				g.setColor(modele.propositions[y].getColor(x));
-                        g.fillOval(10+x*size*2, 10+y*size*2, size, size);
-        			}
-        		}
-        	}
-        	
-        
+            if (modele.propositions[y] != null) {
+                for (int x = 0; x < modele.DIFFICULTE; x++) {
+                    if (modele.propositions[y].getColor(x) != null) {
+                        g.setColor(modele.propositions[y].getColor(x));
+                        g.fillOval(10 + x * size * 2, 10 + y * size * 2, size, size);
+                    }
+                }
+            }
         }
-        System.out.println("--------------------------------------------------------------");
-        
-      }
+    }
+
      
     public VuePropositions(Modele modele) {
          
