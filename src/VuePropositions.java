e596f1fe15fff8be152167f9ddc9d6464d9ca18d
RemyGizard <130280613+RemyGizard@users.noreply.github.com>
2024-03-07 10:17:33 +0100

    Update VuePropositions.java

--- src/VuePropositions.java
+++ src/VuePropositions.java
@@ -6,23 +6,40 @@ public class VuePropositions extends JPanel {
     Color[][] couleur_jouer;
      
      
-    public void paint(Graphics g) {
-        int size = 100 / modele.DIFFICULTE;
+    public void paint(Graphics g){
+         
+         
+        int size= 100/modele.DIFFICULTE;
+        g.setColor(new Color(80,80,100));
+        g.fillRect(0, 0, 200, 500);
+        
         g.setColor(Color.BLACK);
         g.drawRect(0, 0, 200, 500);
-
         for (int y = 0; y < 10; y++) {
-            if (modele.propositions[y] != null) {
-                for (int x = 0; x < modele.DIFFICULTE; x++) {
-                    if (modele.propositions[y].getColor(x) != null) {
-                        g.setColor(modele.propositions[y].getColor(x));
-                        g.fillOval(10 + x * size * 2, 10 + y * size * 2, size, size);
-                    }
-                }
-            }
+        	System.out.println(modele.propositions[y]);
+        	if ((modele.propositions[y] != null)) {
+      
+        		for (int x=0; x < modele.DIFFICULTE; x++) {
+        			if (modele.propositions[y].getColor(x)!=null) {
+        				g.setColor(modele.propositions[y].getColor(x));
+                        g.fillOval(10+x*size*2, 10+y*size*2, size, size);
+        			}
+        			
+        			if ((modele.propositions[y].getColor(x)!=null) && (x==3)) {
+        				for (int p=0; p < modele.DIFFICULTE; p++) {
+        					g.setColor(Color.BLACK);
+                            g.fillOval(220+p%2*size/2, 10+y*size*2+(p/2)*size/2, size/2, size/2);
+        				}
+        				
+        			}
+        		}
+        	}
+        	
+        
         }
-    }
-
+        System.out.println("--------------------------------------------------------------");
+        
+      }
      
     public VuePropositions(Modele modele) {
          
